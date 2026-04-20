9762f7336361e1f7ed096564fda8134d43601004
Otaviofertig <otavio.lesss@gmail.com>
2026-04-20 20:13:37 -0300

    commit

---
+++ LISTA_DUPALMENTE_ENCADEADA/intro_lista_dupla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct DNode_ DNode {
+    int info;
+    DNode *next;
+    DNode *previous;
+}
+
+int main () {
+
+    DNode *node1 = (DNode*) malloc(sizeof(DNode));
+    DNode *node2 = (DNode*) malloc(sizeof(DNode));
+    DNode *node3 = (DNode*) malloc(sizeof(DNode));
+
+    node1->info = 10;
+    node2->info = 20;
+    node3->info = 30;
+
+    node1->previous = NULL;
+    node1->next = node2;
+
+    node2->previous = node1;
+    node2->next = node3;
+
+    node3->previous = node2;
+    node3->next = NULL;
+}
