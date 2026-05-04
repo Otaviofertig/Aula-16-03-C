ebad0c46d69c3aac8270a30b2184b05f9f0054f0
Otaviofertig <204613470+Otaviofertig@users.noreply.github.com>
2026-05-04 20:13:10 -0300

    commit arrumei

---
+++ Ordenador/SelectionSort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void selectionSort(int vetor[], int tamanho) {
+    for(int posicao_atual = 0, posicao_atual < tamanho - 1; posicao_atual ++) {
+        int posicao_menor = posicao_atual;
+
+        for(int i = posicao_atual + 1; i < tamanho; i++) {
+            if(vetor[i] < vetor[posicao_menor]){
+                posicao_menor = i;
+            }
+        }
+    }
+
+    if(posicao_menor != posicao_atual) {
+        int auxiliar = vetor[posicao_atual];
+        vetor[posicao_atual] = vetor[posicao_menor];
+        vetor[posicao_menor] = auxiliar;
+    }
+
+}
+
+int main() {
+    int vetor[] = {6,8,15,28,3};
+    int tamanho = sizeof(vetor) / sizeof(vetor[0]);
+
+    selectionSort(vetor, tamanho);
+
+    for(int i=0 ; i<tamanho; i++) {
+        printf("%d", vetor[i]);
+    }
+    return 0;
+}
