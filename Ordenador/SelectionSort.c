a5493d8332b8ba9386e424e98550ee52f89fba90
Otaviofertig <204613470+Otaviofertig@users.noreply.github.com>
2026-05-04 21:09:54 -0300

    commit

--- Ordenador/SelectionSort.c
+++ Ordenador/SelectionSort.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
 
 void selectionSort(int vetor[], int tamanho, int *comparacoes, int *trocas) {
-    for(int posicao_atual = 0, posicao_atual < tamanho - 1; posicao_atual ++) {
+    for(int posicao_atual = 0; posicao_atual < tamanho - 1; posicao_atual++) {
         int posicao_menor = posicao_atual;
 
+      
         for(int i = posicao_atual + 1; i < tamanho; i++) {
+            (*comparacoes)++; 
+            
             if(vetor[i] < vetor[posicao_menor]){
                 posicao_menor = i;
             }
         }
-    }
 
-    if(posicao_menor != posicao_atual) {
-        int auxiliar = vetor[posicao_atual];
-        vetor[posicao_atual] = vetor[posicao_menor];
-        vetor[posicao_menor] = auxiliar;
+      
+        if(posicao_menor != posicao_atual) {
+            int auxiliar = vetor[posicao_atual];
+            vetor[posicao_atual] = vetor[posicao_menor];
+            vetor[posicao_menor] = auxiliar;
+            
+            (*trocas)++; 
+        }
     }
-
 }
 
 int main() {
-    int vetor[] = {6,8,15,28,3};
+    int vetor[] = {6, 8, 15, 28, 3};
     int tamanho = sizeof(vetor) / sizeof(vetor[0]);
     int comparacoes = 0;
     int trocas = 0;
 
-    selectionSort(vetor, tamanho, comparacoes, trocas);
+  
+    selectionSort(vetor, tamanho, &comparacoes, &trocas);
 
-    for(int i=0 ; i<tamanho; i++) {
-        printf("%d", vetor[i]);
+    for(int i = 0 ; i < tamanho; i++) {
+        printf("%d ", vetor[i]); 
     }
+    printf("\n");
+
+    printf("Nº de comparações: %d\n", comparacoes);
+    printf("Nº de trocas: %d\n", trocas);
+
     return 0;
 }
--- Ordenador/bubble.c
+++ Ordenador/bubble.c
@@ -1,23 +1,20 @@
-
-
 #include <stdio.h>
 
 void bubble_sort (int vetor[], int tamanho, int *comparacoes, int *trocas) {
     int houve_troca = 1;
-   
 
     while(houve_troca == 1) {
         houve_troca = 0;
 
-        for(int i=0; i < tamanho -1 ; i++) {
-            comparacoes++;
+        for(int i = 0; i < tamanho - 1; i++) {
+            (*comparacoes)++;
 
             if(vetor[i] > vetor[i + 1]) {
                 int auxiliar = vetor[i];
                 vetor[i] = vetor[i + 1];
                 vetor[i + 1] = auxiliar;
 
-                trocas ++;
+                (*trocas)++;
                 houve_troca = 1;
             }
         }
@@ -25,15 +22,20 @@ void bubble_sort (int vetor[], int tamanho, int *comparacoes, int *trocas) {
 }
 
 int main() {
-    int vetor[] = {6,8,15,28,3};
+    int vetor[] = {6, 8, 15, 28, 3};
     int tamanho = sizeof(vetor) / sizeof(vetor[0]);
     int comparacoes = 0;
     int trocas = 0;
 
-    bubble_sort(vetor, tamanho, comparacoes, trocas);
+    bubble_sort(vetor, tamanho, &comparacoes, &trocas);
 
-    for(int i=0 ; i<tamanho; i++) {
-        printf("%d", vetor[i]);
+    for(int i = 0 ; i < tamanho; i++) {
+        printf("%d ", vetor[i]);
     }
+    printf("\n");
+
+    printf("Nº de comparações: %d\n", comparacoes);
+    printf("Nº de trocas: %d\n", trocas);
+
     return 0;
 }
