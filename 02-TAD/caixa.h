7dc9d97ce628dbc403b6f650d362f8c8c968be42
Otaviofertig <otavio.lesss@gmail.com>
2026-05-11 17:29:34 -0300

    Refactor function signature for destruir_caixa

--- 02-TAD/caixa.h
+++ 02-TAD/caixa.h
@@ -1,10 +1,16 @@
+// Include Guards: Evitam que o arquivo seja incluído duplicadamente,
+// o que causaria erro de "redefinição" no compilador.
 #ifndef CAIXA_H
 #define CAIXA_H
 
-typedef struct Caixa_caixa;
+// O Segredo do TAD (Opaque Pointer): 
+// Declaramos que existe uma struct chamada Caixa, mas NÃO dizemos 
+// o que tem dentro dela. O Cliente (main) fica cego para os dados internos.
+typedef struct Caixa Caixa;
 
+// Assinaturas das funções (O Contrato)
 Caixa* criar_caixa(int valor_inicial);
-void destruir_caixa(Caixa* caixa);
+void destruir_caixa(Caixa* c);
 void escrever_valor(Caixa* c, int valor);
 int ler_valor(Caixa* c);
 
